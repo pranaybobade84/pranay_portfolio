7a506d8e6bb39a4065c463d44ace2beeeab67efc
pranay bobade <pranaybobade84@gmail.com>
2025-07-04 16:57:02 +0530

    fixed connection error

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,9 @@ import { connect } from "mongoose";
 
 export const connectDB = async () => {
   try {
-     const uri = process.env.MONGODB_URI;
+    const uri = process.env.MONGODB_URI;
     if (!uri) throw new Error("MONGODB_URI not found in env");
-
-    console.log("MONGODB_URI",MONGODB_URI)
-    const connectionInstance = await connect(process.env.MONGODB_URI);
+    const connectionInstance = await connect(uri);
 
     console.log(
       `✅ Connected to MongoDB at host: ${connectionInstance.connection.host}`
@@ -17,7 +15,7 @@ export const connectDB = async () => {
       console.error("Application error occurred:", err);
     });
   } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
+    console.error("Failed to connect to MongoDB:", err.stack);
     process.exit(1);
   }
 };
