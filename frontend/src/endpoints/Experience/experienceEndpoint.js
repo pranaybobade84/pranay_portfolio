22c00ca9ed4c05345a36466bd04ddb1abcda700b
pranay bobade <pranaybobade84@gmail.com>
2025-07-20 19:35:43 +0530

    added rtk endpoints for experience

---
+++ frontend/src/endpoints/Experience/experienceEndpoint.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "../apiSlice";
+
+export const experienceApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    addExperience: builder.mutation({
+      query: (body) => ({
+        url: "/experience",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Experience"],
+    }),
+
+    getAllExperience: builder.query({
+      query: () => "/experience",
+      providesTags: ["Experience"],
+    }),
+
+    editExperience: builder.mutation({
+      query: ({ id, ...body }) => ({
+        url: `/experience/${id}`,
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["Experience"],
+    }),
+
+    deleteExperience: builder.mutation({
+      query: (id) => ({
+        url: `/experience/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Experience"],
+    }),
+  }),
+});
+
+export const {
+  useAddExperienceMutation,
+  useGetAllExperienceQuery,
+  useEditExperienceMutation,
+  useDeleteExperienceMutation,
+} = experienceApi;
